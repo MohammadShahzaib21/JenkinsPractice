4c01ebf9f88910aa3dd1ea3575bd389f2f28d394
Mohammad Shahzaib <148314892+MohammadShahzaib21@users.noreply.github.com>
2024-08-31 21:22:48 +0530

    commit

--- Main.java
+++ Main.java
@@ -3,6 +3,6 @@ class Main
     public static void main(String arg)
     {
         System.out.println("Hello Everyone");
-        System.out.println(new Date());
+        System.out.println("How are you all");
     }
 }
