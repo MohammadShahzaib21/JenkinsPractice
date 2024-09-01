c9a731eabe1d1675053718d92f7dad7e314ee50c
Mohammad Shahzaib <148314892+MohammadShahzaib21@users.noreply.github.com>
2024-09-01 08:43:55 +0530

    changes

--- Main.java
+++ Main.java
@@ -6,5 +6,6 @@ class Main
         System.out.println("Hello Everyone");
         System.out.println("How are you all");
         System.out.println(new Date());
+        new student().stud();
     }
 }
---
+++ student.java
@@ -0,0 +1,11 @@
+class student
+{
+    student()
+    {
+        System.out.println("Creating a student class");
+    }
+    void stud()
+    {
+        System.out.println("This is the method of another class");
+    }
+}
